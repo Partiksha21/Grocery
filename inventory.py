7636bebdbd5015cc7d4e210128a4cdae1c071568
Partiksha <kaswanpartiksha@gmail.com>
2025-05-19 22:37:44 +0530

    Add requirements.txt and update Flask app

--- inventory.py
+++ inventory.py
@@ -145,5 +145,8 @@ def checkout():
 
     return render_template("result.html", result=result)
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
